caa7c8cb4f8673b51ba241c6402d4f77e9f07cf7
Gonzalo Daniel Vega <dvega6442@gmail.com>
2026-04-27 12:45:52 -0300

    cambios de redireccion

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,25 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/como-trabajamos",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/es",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/es/",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
